0251a44bd80c3dca82f1bd4a712ea5b7a66b31a2
Juan Mario Rodriguez <oasistech@Juans-MacBook-Pro.local>
2022-10-10 19:33:11 -0500

    update proxy and endpoint env

--- frontend/src/components/SuccessfulPayment.js
+++ frontend/src/components/SuccessfulPayment.js
@@ -59,7 +59,7 @@ const SuccessfulPayment = () => {
                     break;
             }
         });
-    }, [stripe]);
+    }, [stripe, dispatch]);
     return (
         <>
             <div>
